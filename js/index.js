06d0dabd58a041aae5db4704d0d8dea7178edd49
Dmitry <dmitry.olefir@novidea.com>
2023-12-19 21:37:34 +0200

    updated logic

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 function runHW() {
+  document.getElementById("arr").innerHTML = "";
+  document.getElementById("sum1").innerHTML = "";
+
   const userInput = prompt('Enter elements of list separating them by ",".');
 
   if (userInput === null) alert("Ok, bye");
-  else if (!userInput) alert("Nothing entered!");
+  else if (!userInput.trim()) alert("Nothing entered!");
   else calculateSum(userInput);
 }
 
@@ -11,26 +14,23 @@ function calculateSum(userInput) {
   document.getElementById("arr").innerHTML = "[ " + userArr.join(", ") + " ]";
 
   let sum1 = 0;
-  let sum2 = 0;
   for (const currEl of userArr) {
-    if (canConvert2Number(currEl)) sum1 += Number(currEl.trim());
-    if (canConvert2NumberStartsWithDigits(currEl))
-      sum2 += parseFloat(currEl.trim());
+    if (canConvert2Number(currEl)) {
+      sum1 += Number(currEl.trim());
+    } else {
+      alert("Bad value!");
+      return;
+    }
   }
   document.getElementById("sum1").innerHTML = sum1;
-  document.getElementById("sum2").innerHTML = sum2;
 }
 
 function canConvert2Number(el) {
-  if (el === null || !el.trim() || !Number(el.trim())) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function canConvert2NumberStartsWithDigits(el) {
-  if (el === null || !el.trim() || !parseFloat(el.trim())) {
+  if (
+    el === null ||
+    !el.trim() ||
+    (Number(el.trim()) !== 0 && !Number(el.trim()))
+  ) {
     return false;
   } else {
     return true;
